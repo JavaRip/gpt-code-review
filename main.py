93f7db4577bdf4c7e8ef1578b18bfe2a34a90f42
JavaRip <up941226@myport.ac.uk>
2023-11-18 11:47:49 +0000

    flesh out action & script

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
+import requests
 import argparse
+import os
 
-def main(pr_number, src_commit_id, dest_commit_id):
+OPEN_API_KEY = os.environ.get('OPEN_API_KEY')
+TOKEN = os.environ.get('GITHUB_TOKEN')
+
+def post_comment_to_pr(repo, pr_number, comment):
+    url = f"https://api.github.com/repos/{repo}/issues/{pr_number}/comments"
+    headers = {
+        "Authorization": f"token {TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        "body": comment
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    return response
+
+def main(pr_number, src, dest, repo):
     print(f'pr_number: {pr_number}')
-    print(f'src_commit_id: {src_commit_id}')
-    print(f'dest_commit_id: {dest_commit_id}')
+    print(f'src_commit_id: {src}')
+    print(f'dest_commit_id: {dest}')
+    print(f'dest_commit_id: {repo}')
 
+    post_comment_to_pr(
+      repo,
+      pr_number,
+      f'pr_number: {pr_number}\nsrc_commit_id: {src}\ndest_commit_id: {dest}\nrepo: {repo}'
+    )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -27,5 +51,16 @@ if __name__ == '__main__':
       help='the pr number to append the output to as a comment',
     )
 
+    parser.add_argument(
+      'repo',
+      type=str,
+      help='the repo to run the script on',
+    )
+
     args = parser.parse_args()
-    main(args.pr, args.src, args.dest)
+    main(
+      args.pr,
+      args.src,
+      args.dest,
+      args.repo,
+    )
