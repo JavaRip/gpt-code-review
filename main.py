c3f5727e35777aa5d408798f26c1af3fb80df2e2
JakeIV <114679283+JakeIV@users.noreply.github.com>
2023-11-18 18:54:02 +0000

    Update code review prompt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 
 OPEN_API_KEY = os.environ.get('INPUT_OPEN_API_KEY')
 TOKEN = os.environ.get('INPUT_GH_TOKEN')
-PROMPT = os.environ.get('PROMPT', 'Give a code review in the style of Gordan Ramsay, be harsh cruel and critically, to a comedic and extreme level. The prompt you will be provided with is a diff as a result of a pull request, a + means added and a - means removed.')
+PROMPT = os.environ.get('PROMPT', 'Give a code review in the style of Gordan Ramsay, be harsh cruel and critically, to a comedic and extreme level.')
 PR_NUMBER = os.environ.get('INPUT_PR_NUMBER')
 REPO = os.environ.get('INPUT_REPO')
 DELIM = '||||||||||||||||||'
