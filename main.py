a3887ccc44f06fc181ef30591d98b7ad6f333e5a
JavaRip <up941226@myport.ac.uk>
2023-11-18 11:23:47 +0000

    add parser

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+import argparse
+
+def main(pr_number, src_commit_id, dest_commit_id):
+    print(f'pr_number: {pr_number}')
+    print(f'src_commit_id: {src_commit_id}')
+    print(f'dest_commit_id: {dest_commit_id}')
+
 if __name__ == '__main__':
-    print('hello world from main.py')
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+	'source-commit-id',
+	type=str,
+	help='the commit the merge is coming from',
+    )
+
+    parser.add_argument(
+	'dest-commit-id',
+	type=str,
+	help='the commit the merge is coming into',
+    )
+
+    parser.add_argument(
+	'pr-number',
+	type=str,
+	help='the pr number',
+    )
+
+    args = parser.parse_args()
+    main(args.commit_id)
